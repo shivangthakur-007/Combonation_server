478c1104836808c1e3bb56744de79a4d52066bcf
Dance <thakurshivang579@gmail.com>
2024-01-02 22:21:02 +0530

    first to go on render

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import productRouter from './routers/product.route.js';
+import errorMiddleware from './middleware/error.middleware.js';
+import morgan from 'morgan';
 
 const app= express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+
+app.use(morgan("dev"));
 
 app.use('/ping', (req, res)=>{
     res.send('pong')
@@ -14,5 +18,5 @@ app.use('/api/vi/product', productRouter);
 app.all('*', (req, res)=>{
     res.status(400).send('OOPS! Page not Found')
 })
-
+app.use(errorMiddleware)
 export default app;
